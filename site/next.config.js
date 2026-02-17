939aec64ea4b0e8d5080093e10528311f4a594e6
ThomasSteinerPrivat <63325137+zaybon@users.noreply.github.com>
2026-02-17 01:15:27 +0100

    Update basePath in next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://tst.wtf',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.1',
+  basePath: '/kasm_workspaces_registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
