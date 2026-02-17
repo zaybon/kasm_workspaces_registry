4eb2ab764ac24d506ede400f04c936502d579604
ThomasSteinerPrivat <63325137+zaybon@users.noreply.github.com>
2026-02-17 01:14:05 +0100

    Update configuration URLs and base path

--- site/next.config.js
+++ site/next.config.js
@@ -7,11 +7,11 @@ const nextConfig = {
     name: 'Methotec KASM Registry',
     description: 'The un-official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://zaybon.github.io/kasm_workspaces_registry/',
     contactUrl: 'https://tst.wtf',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
